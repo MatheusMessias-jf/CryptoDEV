cc95a89aff6ddaf8a3cf3d1946429030dcc4d8d2
Matheus Messias <matheus.jf@live.com>
2022-10-17 23:58:34 -0300

    add toggle

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggle() {
+    var texto = document.getElementById("auxContact")
+    
+    if(texto.style.display == "block"){
+        texto.style.display = "none"
+    }
+    else {
+        texto.style.display = "block"
+    }
+}
